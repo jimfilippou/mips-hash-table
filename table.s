ef69a7f6eaa875a2bd46c39c1dd455981d2aecb3
Jim Filippou <jimfilippou8@gmail.com>
2019-12-17 14:15:26 +0200

    fix indents?

--- table.s
+++ table.s
@@ -2,29 +2,29 @@
 # 3160242 - Leonidas Velentzas
 
 .data
-	crlf:		.asciiz	"\n"
+	crlf:			.asciiz	"\n"
 	.align 2
-	hash:		.space	40
-	N:		.word	10
-	key:		.word	0
-	keys:		.word	0
-	pos:		.word	0
-	choice:		.word	0
-	telos:		.word	0
-	menu:		.asciiz "Menu\n1.Insert key\n2.Find key\n3.Display Hash Table\n4.Exit\n"
+	hash:			.space	40
+	N:				.word	10
+	key:			.word	0
+	keys:			.word	0
+	pos:			.word	0
+	choice:			.word	0
+	telos:			.word	0
+	menu:			.asciiz "Menu\n1.Insert key\n2.Find key\n3.Display Hash Table\n4.Exit\n"
 	diplayTemplate:	.asciiz	"\npos key\n"
-	choose:		.asciiz "\nChoose operation:"
+	choose:			.asciiz "\nChoose operation:"
 
 .text
 .globl main
 
 main:
 	
-	lw	$s0,	N		# N    = 10 
+	lw	$s0,	N			# N    = 10 
 	lw 	$s1,	keys		# keys = 0
 	
-	lw	$t0,	key		# key    = 0
-	lw 	$t1,	pos		# pos    = 0
+	lw	$t0,	key			# key    = 0
+	lw 	$t1,	pos			# pos    = 0
 	lw 	$t2,	choice		# choice = 0
 	lw 	$t3,	telos		# telos  = 0
 	
@@ -61,13 +61,13 @@ continue:
 	
 	# Start control flow
 	addi	$t4,	$zero,	1
-	beq	$t7,	$t4,	insert			# if (choice == 1) go to insert
+	beq		$t7,	$t4,	insert			# if (choice == 1) go to insert
 	addi	$t4,	$zero,	2
-	beq	$t7,	$t4,	findKey			# if (choice == 2) go to find
+	beq		$t7,	$t4,	findKey			# if (choice == 2) go to find
 	addi	$t4,	$zero,	3
-	beq	$t7,	$t4,	displayTable	# if (choice == 3) go to show
+	beq		$t7,	$t4,	displayTable	# if (choice == 3) go to show
 	addi	$t4,	$zero,	4
-	beq	$t7,	$t4,	terminate		# if (choice == 4) exit;
+	beq		$t7,	$t4,	terminate		# if (choice == 4) exit;
 	# End control flow
 	
 	
@@ -87,16 +87,16 @@ displayTable:
 	addi	$t4,	$zero,	0
 	addi	$t5,	$zero,	0
 	displayFor:
-		beq	$t4,	$s0,	terminate
-		lw	$t6,    hash($t5)
-		li	$v0,	1
+		beq		$t4,	$s0,	terminate
+		lw		$t6,    hash($t5)
+		li		$v0,	1
 		move	$a0,	$t4
 		syscall
-		li	$v0,	1
+		li		$v0,	1
 		move	$a0,	$t6
 		syscall
-		li	$v0,	4
-		la	$a0,	crlf
+		li		$v0,	4
+		la		$a0,	crlf
 		syscall
 		addi	$t4,	$t4,	1
 		addi	$t5,	$t5,	4
