cf04192fcce39794f1d8119dc51929d64dfa5b22
Jim Filippou <jimfilippou8@gmail.com>
2019-12-24 16:46:08 +0200

    preparations for insert

--- 3160253_3160242.asm
+++ 3160253_3160242.asm
@@ -1,5 +1,8 @@
-# 3160253 - Dimitris Filippou
-# 3160242 - Leonidas Velentzas
+# *------------------------------*
+# | Authors:                     |
+# | 3160253 - Dimitris Filippou  |
+# | 3160242 - Leonidas Velentzas |
+# *------------------------------*
 
 .data
     crlf:           .asciiz "\n"
@@ -14,25 +17,26 @@
     menu:           .asciiz "Menu\n1.Insert key\n2.Find key\n3.Display Hash Table\n4.Exit\n"
     diplayTemplate: .asciiz	"\npos key\n"
     choose:         .asciiz "\nChoose operation:"
-    askKey:         .asciiz "Give key to search for"
+    askKey:         .asciiz "Give key to search for: "
     notInTableStr:  .asciiz "Key not in hash table.\n"
+    giveKey:        .asciiz "Give a new key (greater than zero): "
+    lessThanZero:   .asciiz "Key must be greater than zero"
 
 .text
 .globl main
 
 main:
 
-    lw      $s0,    N      # N    = 10
-    lw      $s1,    keys   # keys = 0
+    lw      $s0,    N                   # N    = 10
+    lw      $s1,    keys                # keys = 0
+    lw      $t0,    key                 # key    = 0
+    lw      $t1,    pos                 # pos    = 0
+    lw      $t2,    choice              # choice = 0
+    lw      $t3,    telos               # telos  = 0
 
-    lw      $t0,    key    # key    = 0
-    lw      $t1,    pos    # pos    = 0
-    lw      $t2,    choice # choice = 0
-    lw      $t3,    telos  # telos  = 0
-
-    addi    $t4,    $zero,  0   # $t4 = 0 (for counter)
-    addi    $t5,    $zero,  0   # $t5 = 0 (index for array, adds 4 every time)
-    addi    $t6,    $zero,  0   # $t6 = 0 (used for filling array, never changes)
+    addi    $t4,    $zero,  0           # $t4 = 0 (for counter)
+    addi    $t5,    $zero,  0           # $t5 = 0 (index for array, adds 4 every time)
+    addi    $t6,    $zero,  0           # $t6 = 0 (used for filling array, never changes)
 
 for:
     bge     $t4,    $s0,    continue	# Check condition of 'for'
@@ -44,30 +48,51 @@ for:
 continue:
 
     # Print the menu
-    li      $v0,    4
     la      $a0,    menu
-    syscall
+    jal     print
 
     # Get user input
-    li      $v0,    4
     la      $a0,    choose
-    syscall
+    jal     print
+
     li	    $v0,    5       # Read user input
     syscall
+
     move    $t7,    $v0     # $t7 = readInt();
 
     # At this point, $t4 + $t5 + $t6 are available for use
 
-    # Start control flow
+    # *--------------------*
+    # | Start control flow |
+    # *--------------------*
     addi    $t4,    $zero,  1
-    beq     $t7,    $t4,    insert
+    beq     $t7,    $t4,    prepareToInsert
     addi    $t4,    $zero,  2
     beq     $t7,    $t4,    prepareToFindKey
     addi    $t4,    $zero,  3
     beq     $t7,    $t4,    prepareToDisplayTable
     addi    $t4,    $zero,  4
     beq     $t7,    $t4,    terminate
-    # End control flow
+    # *--------------------*
+    # |  End control flow  |
+    # *--------------------*
+
+    prepareToInsert:
+        la      $a0,    giveKey
+        jal     print
+        jal     readInt
+        move    $t4,    $v0
+        blt     $t4,    $zero,    keyLessThanZero
+
+        move    $a0,    $t4    # Supply argument
+        jal     insert
+        
+        keyLessThanZero:
+            la    $a0,    lessThanZero
+            jal   print
+        
+        j       continue
+
 
     prepareToDisplayTable:
         lw      $a0,    hash
@@ -75,25 +100,23 @@ continue:
         j       continue
     
     prepareToFindKey:
-        li      $v0,    4
+
         la      $a0,    askKey
-        syscall
+        jal     print
+
         li      $v0,    5
         syscall
-        move    $t4,    $v0
-        
-        lw      $a0,    hash    # Pass argument: hash
-        move    $a1,    $t4     # Pass argument: key to find
-        jal     findKey
-        
-        move    $t8,    $v0     # Store output to $t8
 
-        beq     $t8,    -1,    notInTable
+        move    $t4,    $v0
+        lw      $a0,    hash               # Pass argument: hash
+        move    $a1,    $t4                # Pass argument: key to find
+        jal     findKey                    # Execute findKey
+        move    $t8,    $v0                # Store output to $t8
+        beq     $t8,    -1,    notInTable  # If ($t8 == -1)
 
         notInTable:
-            li      $v0,    4
             la      $a0,    notInTableStr
-            syscall
+            jal     print
 
 
     # At this point, $t4 is available for use
@@ -102,6 +125,7 @@ continue:
 
 
 insert:
+    jr    $ra
 
 findKey:   
     addi    $t4,    $zero,    0             # $t4 = 0 (position)
@@ -147,12 +171,14 @@ displayTable:
         li      $v0,    1
         move    $a0,    $t4
         syscall
+
         li      $v0,    1
         move    $a0,    $t6
         syscall
-        li      $v0,    4
+        
         la      $a0,    crlf
-        syscall
+        jal     print
+
         addi    $t4,    $t4,    1
         addi    $t5,    $t5,    4
         j       displayFor
@@ -160,6 +186,23 @@ displayTable:
         jr     $ra
 
 
+# *------------------*
+# | Helper functions |
+# *------------------*
+
+print:
+    li      $v0,    4
+    syscall
+    jr      $ra
+
+readInt:
+    li      $v0,    5
+    syscall
+    jr      $ra
+
+# *------------------*
+# | End of execution |
+# *------------------*
 terminate:
     li    $v0,    10
     syscall
